c3ad832d6c4f471710835c2f9142eae450f600ec
victor.alvarado <victor.alvarado@mh.gob.sv>
2019-05-07 13:41:02 -0600

    arreglado combos

--- src/main/java/mj/gob/sisadmrh/controller/empleado/EmpleadoController.java
+++ src/main/java/mj/gob/sisadmrh/controller/empleado/EmpleadoController.java
@@ -2,6 +2,7 @@ package mj.gob.sisadmrh.controller.empleado;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import mj.gob.sisadmrh.controller.UtilsController;
@@ -11,6 +12,7 @@ import mj.gob.sisadmrh.model.Contacto;
 import mj.gob.sisadmrh.model.Contrato;
 import mj.gob.sisadmrh.model.Dependiente;
 import mj.gob.sisadmrh.model.Empleado;
+import mj.gob.sisadmrh.model.Estado;
 import mj.gob.sisadmrh.model.Experiencialaboral;
 import mj.gob.sisadmrh.model.Formacionacademica;
 import mj.gob.sisadmrh.model.Hijodiscapacidad;
@@ -33,12 +35,14 @@ import mj.gob.sisadmrh.service.UbicacionFisicaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.support.SessionStatus;
 /**
  *
  * @author dialv
@@ -186,8 +190,14 @@ public class EmpleadoController extends UtilsController {
     }
 
     @RequestMapping(value = "empleado")
-    public String saveEmpleado(@Valid Empleado empleado, Model model) {//,SessionStatus status
+    public String saveEmpleado(@Valid Empleado empleado,BindingResult result, Model model,SessionStatus status) {//,SessionStatus status
         try {
+            
+            Estado municipio = estadoService.getEstadoById(Integer.parseInt(empleado.getMunicipionacimiento())).get();
+            Estado depto = estadoService.getEstadoById(Integer.parseInt(municipio.getCodigoestadosuperior())).get();
+           
+            empleado.setDepartamentonacimiento(depto.getNombreestado());
+            empleado.setMunicipionacimiento(municipio.getNombreestado());
             empleadoService.saveEmpleado(empleado);
             model.addAttribute("msg", 0);
         } catch (Exception e) {
